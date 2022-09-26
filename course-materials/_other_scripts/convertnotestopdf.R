e5e37b552c4620979ea239801fa0cc16df52dece
Tyler George <82473808+stats-tgeorge@users.noreply.github.com>
2022-09-26 18:24:27 -0500

    day 2 updates
    notes, lab 01 fix

--- course-materials/_other_scripts/convertnotestopdf.R
+++ course-materials/_other_scripts/convertnotestopdf.R
@@ -2,10 +2,7 @@ library(xaringanBuilder)
 
 setwd("C:/Users/tgeorge/Desktop/website/course-materials/_slides")
 
-slide_name = "u1-d03-toolkit-git/u1-d03-toolkit-git"
-
-u1-d02-toolkit-r
-u1-d03-toolkit-git
+slide_name = "u2-d04-viz-cat/u2-d04-viz-cat"
 
 # This sometimes needs run twice
 #build_pdf(paste0(slide_name,'.rmd'), complex_slides = TRUE, partial_slides = TRUE)
