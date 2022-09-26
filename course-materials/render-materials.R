2e768c83c92d133b28e813ebf1be111b26e5f506
Tyler George <82473808+stats-tgeorge@users.noreply.github.com>
2022-09-25 21:21:38 -0500

    Update render-materials.R

--- course-materials/render-materials.R
+++ course-materials/render-materials.R
@@ -7,14 +7,15 @@ library(xaringan)
 
 # non-xaringan -----------------------------------------------------------------
 
-rmds <- dir_info(recurse = 3, glob = "course-materials/*.Rmd") %>% 
+
+rmds <- dir_info(recurse = 3, glob = "course-materials/*.Rmd") %>%
   filter(!str_detect(path, "slides")) %>%
   pull(path)
 walk(rmds, render)
 
 # xaringan ---------------------------------------------------------------------
 
-xaringans <- dir_info(recurse = 3, glob = "course-materials/*.Rmd") %>% 
+xaringans <- dir_info(recurse = 3, glob = "course-materials/*.Rmd") %>%
   filter(str_detect(path, "_slides")) %>%
   filter(!str_detect(path, "setup")) %>%
   pull(path)
