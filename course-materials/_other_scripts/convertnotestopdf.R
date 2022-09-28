29568bdbc7a04980f318b57fe413638629c95734
Tyler George <82473808+stats-tgeorge@users.noreply.github.com>
2022-09-28 17:45:26 -0500

    updated day 4 content
    added a few ethics discussions, updated notes d2  5-9 and made a few branches on site.

--- course-materials/_other_scripts/convertnotestopdf.R
+++ course-materials/_other_scripts/convertnotestopdf.R
@@ -2,12 +2,13 @@ library(xaringanBuilder)
 
 setwd("C:/Users/tgeorge/Desktop/website/course-materials/_slides")
 
-slide_name = "u2-d04-viz-cat/u2-d04-viz-cat"
+slide_name = "u2-d09-tidying"
+slide_name = paste0(slide_name,"/",slide_name)
 
 # This sometimes needs run twice
 #build_pdf(paste0(slide_name,'.rmd'), complex_slides = TRUE, partial_slides = TRUE)
 build_pdf(paste0(slide_name,'.html'), complex_slides = TRUE, partial_slides = TRUE)
 
 # OR dektape
-system("`npm bin`/decktape remark slides.html slides.pdf") #
-xaringan::decktape()
+#system("`npm bin`/decktape remark slides.html slides.pdf") #
+#xaringan::decktape()
